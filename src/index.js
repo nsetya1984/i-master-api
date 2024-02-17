41e7dffcd0a3aa50fd5401fceb78ee5226d90c2a
nswardana <nswardana@gmail.com>
2024-02-17 18:55:06 +0800

    portray ga mau

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const { logger } = require('./utils/logger');
 
-const PORT = process.env.PORT || 200;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     logger.info(`Running on PORT ${PORT}`);
