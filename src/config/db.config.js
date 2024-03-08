c553effeb2b5d156a9c6314dcd0293bb0d4c04ee
nswardana <nswardana@gmail.com>
2024-03-08 19:19:05 +0800

    no message

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -4,9 +4,9 @@ const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = require('../utils/secrets');
 
 const connection = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "",
-    database: 'i-project'
+    user: "premis",
+    password: "DTCMtolong313",
+    database: 'premis'
 });
 
 connection.connect((err) => {
