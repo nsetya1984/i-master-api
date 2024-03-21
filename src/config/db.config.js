ff3cc2456dc0754f9b76d8a6baf40ff21e1a9bff
nswardana <nswardana@gmail.com>
2024-03-21 11:43:53 +0800

    perbaikan setting database

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,13 +1,27 @@
 const mysql = require('mysql');
 const { logger } = require('../utils/logger');
 const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = require('../utils/secrets');
+const os = require('os'); 
+var connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: 'i-project'
+    });
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "premis",
-    password: "DTCMtolong313",
-    database: 'premis'
-});
+
+if(os.hostname() !== "Nanangs-MacBook-Pro.local")
+{
+
+    console.log("SERVER");
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "premis",
+        password: "DTCMtolong313",
+        database: 'premis'
+    });
+
+}
 
 connection.connect((err) => {
     if (err) logger.error(err.message);
